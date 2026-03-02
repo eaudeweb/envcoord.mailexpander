da3a0cb063d972e0633c2adf6c6cabbfe9f29125
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2026-03-02 13:37:52 +0200

    remove tracebacklimit to allow tracebacks on errors

--- envcoord/mailexpander/expander.py
+++ envcoord/mailexpander/expander.py
@@ -75,7 +75,6 @@ X-Actually-To: test-administration@envcoord.health.fgov.be
 
 The item "Test" has been edited."""
 
-sys.tracebacklimit = 0
 log = logging.getLogger('rolesexpander')
 log.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler()
